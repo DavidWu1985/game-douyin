cd8ceaff77402ddbd256dac040c64677a90628d6
David <since1985rh@163.com>
2023-02-22 21:00:52 +0800

    gameservice改成wdgm

--- src/main/java/com/wd803/game/douyin/controller/GameServiceController.java
+++ src/main/java/com/wd803/game/douyin/controller/GameServiceController.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/gameservice/*")
+@RequestMapping("/wdgm/*")
 public class GameServiceController {
 
     @Autowired
